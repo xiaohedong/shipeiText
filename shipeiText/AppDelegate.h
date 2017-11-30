b469e88c4bed93739ec026739013f276818e4d31
hongbaodai <dongxiaohe@hongbaodai.com>
2017-11-30 10:39:45 +0800

    Initial Commit

---
+++ shipeiText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shipeiText
+//
+//  Created by hongbaodai on 2017/11/30.
+//  Copyright © 2017年 hongbaodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shipeiText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shipeiText
+//
+//  Created by hongbaodai on 2017/11/30.
+//  Copyright © 2017年 hongbaodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
